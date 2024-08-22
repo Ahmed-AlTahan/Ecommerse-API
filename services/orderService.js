83242e9e25127c0fa9cf50e621ca9370418cfa40
Ahmad <eng.ahmed.tahan2@gmail.com>
2024-08-22 16:38:20 +0300

    fix error

--- services/orderService.js
+++ services/orderService.js
@@ -153,7 +153,7 @@ exports.webhookCheckout = asyncHandler(async(req, res, next) => {
 
     let event;
     try {
-        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
         console.log(err);
         return res.status(400).send(`Webhook Error: ${err.message}`);
